2c523d40801ae79452d28cd576dcbed2613ad4df
Philipp <www.phill.999@gmail.com>
2021-02-10 11:34:14 +0300

    add login

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from app.models import Item, Supplier, Category
+from app.models import Item, Supplier, Category, UserAccount
 
 
 @admin.register(Category)
@@ -16,3 +15,22 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
     pass
+
+def set_active_user(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+set_active_user.short_description = "Сделать активными"
+
+def set_passive_user(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+set_passive_user.short_description = "Сделать неактивными"
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'is_active']
+    ordering = ['is_active']
+    list_filter = ('is_active',)
+    actions = [set_active_user, set_passive_user]
+    search_fields = ['first_name', 'last_name',]
+
+admin.site.register(UserAccount, ListAdmin)
--- app/views/common.py
+++ app/views/common.py
@@ -1,8 +1,32 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
-    ctx = {
-        'title': 'Home'
-    }
-    return render(request, 'index.html', ctx)
+    if request.user.is_authenticated:
+        ctx = {
+            'title': 'Home'
+        }
+        return render(request, "index.html", ctx)
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        UserAccount = authenticate(request, username=email, password=password)
+        # Check if authentication successful
+        if UserAccount is not None:
+            login(request, UserAccount)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "login.html")
